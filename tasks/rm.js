052eb52a9708152c6e66c4d3c8aefb5c35eef9fb
André Cruz <andremiguelcruz@msn.com>
2012-11-13 13:33:25 +0000

    Complete rm task.

--- tasks/rm.js
+++ tasks/rm.js
@@ -1,14 +1,25 @@
+var rimraf = require('rimraf');
+
 var task = {
     'id'     : 'rm',
     'author' : 'Indigo United',
     'name'   : 'Remove',
+    'options' : {
+        'file': {
+            'description': 'The file or directory you want to remove'
+        }
+    },
     'tasks'  :
     [
         {
             'task' : function (opt, next) {
-                // TODO: implement this
-
-                next();
+                rimraf(opt.file, function (err) {
+                    if (err) {
+                        next(err);
+                    } else {
+                        next();
+                    }
+                });
             }
         }
     ]
