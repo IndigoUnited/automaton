43dab1c88124468d3dd807d16043c4c9d1d8bd36
filipediasferreira <filipediasferreira@gmail.com>
2012-12-04 21:30:33 +0000

    Mkdir :: support to multiple dirs and mode

--- tasks/mkdir.js
+++ tasks/mkdir.js
@@ -1,5 +1,7 @@
 var mkdirp = require('mkdirp');
 var fs     = require('fs');
+var utils  = require('amd-utils');
+var async  = require('async');
 
 var task = {
     'id'      : 'mkdir',
@@ -8,33 +10,58 @@ var task = {
     'options' : {
         'dir': {
             'description': 'The directory you want to create'
+        },
+        'mode': {
+            'description': 'The directory permissions',
+            'default': '0777'
+
         }
     },
-    'tasks'   :
+    'filter': function (opt) {
+        if (!utils.lang.isNumber(opt.mode)) {
+            opt.mode = parseInt(opt.mode, 8);
+        }
+    },
+    'tasks' :
     [
         {
             'task' : function (opt, next) {
-                fs.stat(opt.dir, function (err, stat) {
-                    if (!err || err.code !== 'ENOENT') {
-                        if (stat && !stat.isDirectory()) {
-                            next(new Error('Passed dir already exists and is not a directory.'));
-                        } else {
-                            next(err);
-                        }
-                    }
 
-                    mkdirp(opt.dir, function (err) {
-                        if (err) {
-                            next(err);
-                        }
-                        else {
-                            next();
-                        }
-                    });
-                });
+                if (utils.lang.isString(opt.dir)) {
+                    opt.dir = [opt.dir];
+                }
+                var tasks = [];
 
+                for (var i = 0, l = opt.dir.length; i < l; ++i) {
+                    tasks.push(function (i) {
+                        fs.stat(opt.dir[i], function (err, stat) {
+                            if (!err || err.code !== 'ENOENT') {
+                                if (stat && !stat.isDirectory()) {
+                                    next(new Error('Passed dir already exists and is not a directory.'));
+                                } else {
+                                    next(err);
+                                }
+                            }
+
+                            mkdirp(opt.dir[i], opt.mode, function (err) {
+                                if (err) {
+                                    next(err);
+                                }
+                                else {
+                                    next();
+                                }
+                            });
+                        });
+                    }.bind(this, i));
+                }
+                async.parallel(tasks, next);
             },
-            description: 'make dir "{{dir}}"'
+            description: 'Make dir'
+            /*
+            description: function (opt) {
+                return 'Making dirs: ' opt.dir.join(',');
+            }*/
+
         }
     ]
 };
