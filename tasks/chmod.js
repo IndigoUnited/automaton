475e4b1039a7327944d801fc413c2e1dad7a2d11
André Cruz <andremiguelcruz@msn.com>
2012-12-07 18:37:57 +0000

    Add chmod task, closes #23.

---
+++ tasks/chmod.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var utils = require('amd-utils');
+var async = require('async');
+var glob  = require('glob');
+
+var task = {
+    id     : 'chmod',
+    author : 'Indigo United',
+    name   : 'Change mode',
+    options: {
+        file: {
+            description: 'The file(s) to chmod'
+        },
+        mode: {
+            description: 'The mode',
+            'default': parseInt('0777', 8)
+        }
+    },
+    filter: function (opt) {
+        if (!utils.lang.isNumber(opt.mode)) {
+            opt.mode = parseInt(opt.mode, 8);
+        }
+    },
+    tasks  :
+    [
+        {
+            task : function (opt, next) {
+                var files = utils.lang.isArray(opt.file) ? opt.file : [opt.file];
+
+                async.forEach(files, function (file, next) {
+                    glob(file, function (err, files) {
+                        if (err) {
+                            next(err);
+                        }
+
+                        async.forEach(files, function (file) {
+                            fs.chmod(file, opt.mode, next);
+                        }, next);
+                    });
+                }, next);
+            }
+        }
+    ]
+};
+
+module.exports = task;
