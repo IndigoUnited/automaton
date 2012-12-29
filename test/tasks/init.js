c0037924787c1778d57479e7514c94021637a38e
filipediasferreira <filipediasferreira@gmail.com>
2012-12-29 12:54:40 +0000

    [wip] Tests

--- test/tasks/init.js
+++ test/tasks/init.js
@@ -1,46 +1,8 @@
-var expect = require('expect.js'),
-    isFile = require('../helpers/util/is-file')
+var expect = require('expect.js')
 ;
 
 module.exports = function (automaton) {
     describe('init', function () {
-        it('should initialize an empty task - with default autofile name', function (done) {
-            var dir      = __dirname + '/../tmp/init/',
-                filename = 'autofile.js';
-
-            automaton.run('init', {
-                dst: dir
-            }, function (err) {
-                if (err) {
-                    return done(err);
-                }
-
-                expect(isFile(dir + filename)).to.be(true);
-                done();
-            });
-
-        });
-
-        it('should initialize an empty task - with specific autofile name', function (done) {
-            var dir      = __dirname + '/../tmp/init/',
-                file     = 'autofile_test.js';
-
-            automaton.run('init', {
-                name: file,
-                dst: dir
-            }, function (err) {
-                if (err) {
-                    return done(err);
-                }
-
-                expect(isFile(dir + file)).to.be(true);
-                done();
-            });
-
-        });
-
-        it.skip('should throw an error if the autofile already exists', function () {
-            // test with dst and withouy dstß
-        });
+        it.skip('should initialize an empty task');
     });
 };
--- test/tasks/symlink.js
+++ test/tasks/symlink.js
@@ -1,8 +1,23 @@
-var expect = require('expect.js')
+var expect = require('expect.js'),
+    isFile = require('../helpers/util/is-file')
 ;
 
 module.exports = function (automaton) {
     describe('symlink', function () {
-        it.skip('should create symlink');
+        it.skip('should create symlink', function (done) {
+
+            var dir      = __dirname + '/../tmp/init/',
+                filename = 'autofile.js';
+
+            automaton.run('symlink', {
+            }, function (err) {
+                if (err) {
+                    return done(err);
+                }
+
+                expect(isFile(dir + filename)).to.be(true);
+                done();
+            });
+        });
     });
 };
