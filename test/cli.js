045dbd2146f81a539569008c9c6ddd0b57a46382
André Cruz <andremiguelcruz@msn.com>
2012-12-29 17:33:14 +0000

    Add very rudimentary tests for the CLI.

--- test/cli.js
+++ test/cli.js
@@ -1,3 +1,18 @@
-module.exports = function (automaton) {
+var cp     = require('child_process');
+var expect = require('expect.js');
 
+module.exports = function () {
+    it('should exit with an appropriate code', function (done) {
+        cp.exec('node bin/automaton test/helpers/tasks/dummy-mandatory', function (err, stdout) {
+            expect(stdout).to.match(/mandatory/);
+        }).on('exit', function (code) {
+            expect(code).to.equal(1);
+
+            cp.exec('node bin/automaton test/helpers/tasks/dummy-mandatory --mandatory=foo')
+              .on('exit', function (code) {
+                expect(code).to.equal(0);
+                done();
+            });
+        });
+    });
 };
