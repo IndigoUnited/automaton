e0bb21bb5aa1e624b075f26c8da96dddf80de720
André Cruz <andremiguelcruz@msn.com>
2013-01-03 21:33:44 +0000

    Add tests for scaffolding-append and scaffolding-replace.

--- test/tasks/scaffolding.js
+++ test/tasks/scaffolding.js
@@ -7,10 +7,12 @@ module.exports = function (automaton) {
         describe('append', function () {
             beforeEach(function () {
                 // Copy assets to the tmp
-                fs.writeFileSync(__dirname + '/../tmp/file1.json', fs.readFileSync(__dirname + '/../helpers/assets/file1.json'));
+                var file1 = fs.readFileSync(__dirname + '/../helpers/assets/file1.json');
+                fs.writeFileSync(__dirname + '/../tmp/file1.json', file1);
+                fs.writeFileSync(__dirname + '/../tmp/file1_copy.json', file1);
             });
 
-            it.skip('should append string to placeholder', function () {
+            it('should append string to placeholder', function (done) {
                 automaton.run({
                     filter: function (opts, next) {
                         opts.__dirname = __dirname;
@@ -20,9 +22,11 @@ module.exports = function (automaton) {
                         {
                             task: 'scaffolding-append',
                             options: {
-                                files: ['{{__dirname}}/../tmp/file1.json'],
+                                files: ['{{__dirname}}/../tmp/file1.json', '{{__dirname}}/../tmp/file1_copy.json'],
                                 data: {
-                                    placeholder: 'awesome '
+                                    placeholder: 'awesome ',
+                                    name: 'André',
+                                    email: 'andre@indigounited.com'
                                 }
                             }
                         }
@@ -32,20 +36,318 @@ module.exports = function (automaton) {
                         throw err;
                     }
 
-                    // TODO validate file
+                    var contents = JSON.parse(fs.readFileSync(__dirname + '/../tmp/file1.json'));
+                    expect(contents.name).to.equal('André{{name}}');
+                    expect(contents.email).to.equal('andre@indigounited.com{{email}}');
+                    expect(contents.some_field).to.equal('This has an awesome {{placeholder}}, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the awesome {{placeholder}} again just in case..');
+
+                    contents = JSON.parse(fs.readFileSync(__dirname + '/../tmp/file1_copy.json'));
+                    expect(contents.name).to.equal('André{{name}}');
+                    expect(contents.email).to.equal('andre@indigounited.com{{email}}');
+                    expect(contents.some_field).to.equal('This has an awesome {{placeholder}}, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the awesome {{placeholder}} again just in case..');
+
+                    done();
                 });
             });
 
-            it.skip('should append file to placeholder');
-            it.skip('should accept minimatch patterns');
-            it.skip('should pass over the glob options');
+            it('should append file to placeholder', function (done) {
+                // Create dummy files
+                fs.writeFileSync(__dirname + '/../tmp/dummy', 'foo');
+                fs.writeFileSync(__dirname + '/../tmp/dummy2', 'bar');
+
+                automaton.run({
+                    filter: function (opts, next) {
+                        opts.__dirname = __dirname;
+                        next();
+                    },
+                    tasks: [
+                        {
+                            task: 'scaffolding-append',
+                            options: {
+                                files: ['{{__dirname}}/../tmp/file1.json', '{{__dirname}}/../tmp/file1_copy.json'],
+                                data: {
+                                    placeholder: '{{__dirname}}/../tmp/dummy',
+                                    name: '{{__dirname}}/../tmp/dummy2',
+                                    email: '{{__dirname}}/../tmp/dummy'
+                                },
+                                type: 'file'
+                            }
+                        }
+                    ]
+                }, null, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    var contents = JSON.parse(fs.readFileSync(__dirname + '/../tmp/file1.json'));
+                    expect(contents.name).to.equal('bar{{name}}');
+                    expect(contents.email).to.equal('foo{{email}}');
+                    expect(contents.some_field).to.equal('This has an foo{{placeholder}}, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the foo{{placeholder}} again just in case..');
+
+                    expect(contents.name).to.equal('bar{{name}}');
+                    expect(contents.email).to.equal('foo{{email}}');
+                    expect(contents.some_field).to.equal('This has an foo{{placeholder}}, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the foo{{placeholder}} again just in case..');
+
+                    done();
+                });
+            });
+
+            it('should accept minimatch patterns', function (done) {
+                automaton.run({
+                    filter: function (opts, next) {
+                        opts.__dirname = __dirname;
+                        next();
+                    },
+                    tasks: [
+                        {
+                            task: 'scaffolding-append',
+                            options: {
+                                files: ['{{__dirname}}/../tmp/file1*.json'],
+                                data: {
+                                    placeholder: 'awesome ',
+                                    name: 'André',
+                                    email: 'andre@indigounited.com'
+                                }
+                            }
+                        }
+                    ]
+                }, null, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    var contents = JSON.parse(fs.readFileSync(__dirname + '/../tmp/file1.json'));
+                    expect(contents.name).to.equal('André{{name}}');
+                    expect(contents.email).to.equal('andre@indigounited.com{{email}}');
+                    expect(contents.some_field).to.equal('This has an awesome {{placeholder}}, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the awesome {{placeholder}} again just in case..');
+
+                    contents = JSON.parse(fs.readFileSync(__dirname + '/../tmp/file1_copy.json'));
+                    expect(contents.name).to.equal('André{{name}}');
+                    expect(contents.email).to.equal('andre@indigounited.com{{email}}');
+                    expect(contents.some_field).to.equal('This has an awesome {{placeholder}}, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the awesome {{placeholder}} again just in case..');
+
+                    done();
+                });
+            });
+
+            it('should pass over the glob options', function (done) {
+                // Rename to .file1 and tell glob to not match files starting with dot
+                fs.renameSync(__dirname + '/../tmp/file1.json', __dirname + '/../tmp/.file1.json');
+
+                automaton.run({
+                    filter: function (opts, next) {
+                        opts.__dirname = __dirname;
+                        next();
+                    },
+                    tasks: [
+                        {
+                            task: 'scaffolding-append',
+                            options: {
+                                files: ['{{__dirname}}/../tmp/*file1.json'],
+                                data: {
+                                    placeholder: 'awesome ',
+                                    name: 'André',
+                                    email: 'andre@indigounited.com'
+                                },
+                                glob: {
+                                    dot: false
+                                }
+                            }
+                        }
+                    ]
+                }, null, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    var contents = JSON.parse(fs.readFileSync(__dirname + '/../tmp/.file1.json'));
+                    expect(contents.name).to.equal('{{name}}');
+                    expect(contents.email).to.equal('{{email}}');
+                    expect(contents.some_field).to.equal('This has an {{placeholder}}, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the {{placeholder}} again just in case..');
+
+                    done();
+                });
+            });
         });
 
         describe('replace', function () {
-            it.skip('should replace placeholder with string');
-            it.skip('should replace placeholder with file');
-            it.skip('should accept minimatch patterns');
-            it.skip('should pass over the glob options');
+            beforeEach(function () {
+                // Copy assets to the tmp
+                var file1 = fs.readFileSync(__dirname + '/../helpers/assets/file1.json');
+                fs.writeFileSync(__dirname + '/../tmp/file1.json', file1);
+                fs.writeFileSync(__dirname + '/../tmp/file1_copy.json', file1);
+            });
+
+            it('should replace placeholder with string', function (done) {
+                automaton.run({
+                    filter: function (opts, next) {
+                        opts.__dirname = __dirname;
+                        next();
+                    },
+                    tasks: [
+                        {
+                            task: 'scaffolding-replace',
+                            options: {
+                                files: ['{{__dirname}}/../tmp/file1.json', '{{__dirname}}/../tmp/file1_copy.json'],
+                                data: {
+                                    placeholder: 'awesome',
+                                    name: 'André',
+                                    email: 'andre@indigounited.com'
+                                }
+                            }
+                        }
+                    ]
+                }, null, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    var contents = JSON.parse(fs.readFileSync(__dirname + '/../tmp/file1.json'));
+                    expect(contents.name).to.equal('André');
+                    expect(contents.email).to.equal('andre@indigounited.com');
+                    expect(contents.some_field).to.equal('This has an awesome, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the awesome again just in case..');
+
+                    contents = JSON.parse(fs.readFileSync(__dirname + '/../tmp/file1_copy.json'));
+                    expect(contents.name).to.equal('André');
+                    expect(contents.email).to.equal('andre@indigounited.com');
+                    expect(contents.some_field).to.equal('This has an awesome, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the awesome again just in case..');
+
+                    done();
+                });
+            });
+
+            it('should replace placeholder with string', function (done) {
+                // Create dummy files
+                fs.writeFileSync(__dirname + '/../tmp/dummy', 'foo');
+                fs.writeFileSync(__dirname + '/../tmp/dummy2', 'bar');
+
+                automaton.run({
+                    filter: function (opts, next) {
+                        opts.__dirname = __dirname;
+                        next();
+                    },
+                    tasks: [
+                        {
+                            task: 'scaffolding-replace',
+                            options: {
+                                files: ['{{__dirname}}/../tmp/file1.json', '{{__dirname}}/../tmp/file1_copy.json'],
+                                data: {
+                                    placeholder: '{{__dirname}}/../tmp/dummy',
+                                    name: '{{__dirname}}/../tmp/dummy2',
+                                    email: '{{__dirname}}/../tmp/dummy'
+                                },
+                                type: 'file'
+                            }
+                        }
+                    ]
+                }, null, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    var contents = JSON.parse(fs.readFileSync(__dirname + '/../tmp/file1.json'));
+                    expect(contents.name).to.equal('bar');
+                    expect(contents.email).to.equal('foo');
+                    expect(contents.some_field).to.equal('This has an foo, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the foo again just in case..');
+
+                    expect(contents.name).to.equal('bar');
+                    expect(contents.email).to.equal('foo');
+                    expect(contents.some_field).to.equal('This has an foo, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the foo again just in case..');
+
+                    done();
+                });
+            });
+
+            it('should accept minimatch patterns', function (done) {
+                automaton.run({
+                    filter: function (opts, next) {
+                        opts.__dirname = __dirname;
+                        next();
+                    },
+                    tasks: [
+                        {
+                            task: 'scaffolding-replace',
+                            options: {
+                                files: ['{{__dirname}}/../tmp/file1*.json'],
+                                data: {
+                                    placeholder: 'awesome',
+                                    name: 'André',
+                                    email: 'andre@indigounited.com'
+                                }
+                            }
+                        }
+                    ]
+                }, null, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    var contents = JSON.parse(fs.readFileSync(__dirname + '/../tmp/file1.json'));
+                    expect(contents.name).to.equal('André');
+                    expect(contents.email).to.equal('andre@indigounited.com');
+                    expect(contents.some_field).to.equal('This has an awesome, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the awesome again just in case..');
+
+                    contents = JSON.parse(fs.readFileSync(__dirname + '/../tmp/file1_copy.json'));
+                    expect(contents.name).to.equal('André');
+                    expect(contents.email).to.equal('andre@indigounited.com');
+                    expect(contents.some_field).to.equal('This has an awesome, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the awesome again just in case..');
+
+                    done();
+                });
+            });
+
+            it('should pass over the glob options', function (done) {
+                // Rename to .file1 and tell glob to not match files starting with dot
+                fs.renameSync(__dirname + '/../tmp/file1.json', __dirname + '/../tmp/.file1.json');
+
+                automaton.run({
+                    filter: function (opts, next) {
+                        opts.__dirname = __dirname;
+                        next();
+                    },
+                    tasks: [
+                        {
+                            task: 'scaffolding-replace',
+                            options: {
+                                files: ['{{__dirname}}/../tmp/*file1.json'],
+                                data: {
+                                    placeholder: 'awesome ',
+                                    name: 'André',
+                                    email: 'andre@indigounited.com'
+                                },
+                                glob: {
+                                    dot: false
+                                }
+                            }
+                        }
+                    ]
+                }, null, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    var contents = JSON.parse(fs.readFileSync(__dirname + '/../tmp/.file1.json'));
+                    expect(contents.name).to.equal('{{name}}');
+                    expect(contents.email).to.equal('{{email}}');
+                    expect(contents.some_field).to.equal('This has an {{placeholder}}, you see?');
+                    expect(contents.other_field).to.equal('Here\'s the {{placeholder}} again just in case..');
+
+                    done();
+                });
+            });
         });
 
         describe('close', function () {
