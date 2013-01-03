8f02f66ccd8f3e0ef160552256618b6f5895a8d7
André Cruz <andremiguelcruz@msn.com>
2013-01-03 20:57:26 +0000

    Tests, WIP.

--- test/tasks/scaffolding.js
+++ test/tasks/scaffolding.js
@@ -1,10 +1,41 @@
-var expect = require('expect.js')
+var expect = require('expect.js'),
+    fs     = require('fs')
 ;
 
 module.exports = function (automaton) {
     describe('scaffolding', function () {
         describe('append', function () {
-            it.skip('should append string to placeholder');
+            beforeEach(function () {
+                // Copy assets to the tmp
+                fs.writeFileSync(__dirname + '/../tmp/file1.json', fs.readFileSync(__dirname + '/../helpers/assets/file1.json'));
+            });
+
+            it.skip('should append string to placeholder', function () {
+                automaton.run({
+                    filter: function (opts, next) {
+                        opts.__dirname = __dirname;
+                        next();
+                    },
+                    tasks: [
+                        {
+                            task: 'scaffolding-append',
+                            options: {
+                                files: ['{{__dirname}}/../tmp/file1.json'],
+                                data: {
+                                    placeholder: 'awesome '
+                                }
+                            }
+                        }
+                    ]
+                }, null, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    // TODO validate file
+                });
+            });
+
             it.skip('should append file to placeholder');
             it.skip('should accept minimatch patterns');
             it.skip('should pass over the glob options');
